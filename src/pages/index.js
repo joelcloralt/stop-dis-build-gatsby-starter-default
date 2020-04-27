3b5f2b7ce6ed90c4b48d85fb285043f3aecd2a77
Joel Cloralt <joel.cloralt@netlify.com>
2020-04-27 09:59:05 -0700

    stopped

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site. Updated Again</p>
+    <p>Welcome to your new Gatsby site. THIS SHOULDNT HAPPEN</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
