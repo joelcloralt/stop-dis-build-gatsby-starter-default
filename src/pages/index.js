007a403c85548fa7acb591f63d051c2be50f23e0
Joel Cloralt <joel.cloralt@netlify.com>
2020-04-27 10:00:14 -0700

    Go

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site. THIS SHOULDNT HAPPEN</p>
-    <p>Now go build something great.</p>
+    <p>Welcome to your new Gatsby site. THIS SHOULD HAPPEN</p>
+    <p>Now go build something great.GO GO GO</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
