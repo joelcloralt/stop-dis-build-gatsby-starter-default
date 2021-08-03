a0544604276e02b6c41585a23968835122de5eeb
Joel Cloralt <joel.cloralt@netlify.com>
2021-08-03 11:40:05 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site. NOTIFY ME PLEASE</p>
       <p>Show me the diff</p>
+      <p>Hello Slack</p>
     <p>Now go build something great.GO GO GO</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
